bf21cb57975332e54d9cd3f51b208f16a4d5c748
budukhyash <budukhyash@gmail.com>
2021-08-26 23:17:45 +0530

    Done

--- app/config/redis.js
+++ app/config/redis.js
@@ -2,7 +2,8 @@ import redis from 'redis'
 
 export const client = redis.createClient({
 	host: process.env.REDIS_HOST,
-	port: process.env.REDIS_PORT
+	port: process.env.REDIS_PORT,
+	password: process.env.REDIS_PASSWORD
 })
 
 client.on('error', (err) => {
